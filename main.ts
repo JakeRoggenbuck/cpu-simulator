4636a80b66c4f458005d3373e3d93da87f4e48b2
Jake <jakeroggenbuck2@gmail.com>
2024-11-08 22:24:56 -0800

    Add opcodes

--- main.ts
+++ main.ts
@@ -6,11 +6,23 @@ function init_state(): State {
   return { PC: 0 };
 }
 
-enum OPCODE {
+export enum OPCODE {
   NOP = 0b0000,
   STOP = 0b0001,
   ADDI = 0b0010,
   ADDR = 0b0011,
+  SUBI = 0b0100,
+  SUBR = 0b0101,
+  ANDI = 0b0110,
+  ANDR = 0b0111,
+  ORI = 0b1000,
+  ORR = 0b1001,
+  XORI = 0b1010,
+  XORR = 0b1011,
+  NEGATE = 0b1100,
+  NOT = 0b1101,
+  MOVE = 0b1110,
+  LOAD = 0b1111,
 }
 
 if (import.meta.main) {
--- main_test.ts
+++ main_test.ts
@@ -1,1 +1,17 @@
-import { assertEquals } from '@std/assert';
+import { assert } from '@std/assert';
+import { OPCODE } from './main.ts';
+
+Deno.test(function opcodeCompletnessTest() {
+  for (let i = 0; i < 16; i++) {
+    assert(OPCODE[i] !== undefined);
+  }
+  assert(OPCODE[15] === 'LOAD');
+
+  assert(OPCODE[14] !== 'LOAD');
+  assert(OPCODE[0] !== 'LOAD');
+
+  assert(OPCODE[0] === 'NOP');
+
+  assert(OPCODE['NOP'] === 0);
+  assert(OPCODE['ADDI'] === OPCODE.ADDI);
+});
