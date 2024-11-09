4fb6cb32caff95c10c911cb9cf9ff437117f4f72
Jake <jakeroggenbuck2@gmail.com>
2024-11-08 22:17:07 -0800

    Add basics

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-export function add(a: number, b: number): number {
-  return a + b;
+interface State {
+  PC: number;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+function init_state(): State {
+  return { PC: 0 };
+}
+
+enum OPCODE {
+  NOP = 0b0000,
+  STOP = 0b0001,
+  ADDI = 0b0010,
+  ADDR = 0b0011,
+}
+
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  let state = init_state();
+
+  console.log(state);
+
+  console.log(OPCODE);
 }
